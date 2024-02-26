2883a55c60dc54ed835a5bf1bbf4b33aeda02e8a
feeld0g <hhy7707@naver.com>
2024-02-26 20:40:42 +0900

    Fix spacing issue

--- pages/base.py
+++ pages/base.py
@@ -8,8 +8,9 @@ class BaseFunction:
 
     def click_by_locator(self, element_locator):
         WebDriverWait(self.driver, 10).until(
-        expected_conditions.visibility_of_element_located(element_locator)).click()
+            expected_conditions.visibility_of_element_located(element_locator)).click()
 
     def find_element_by_locator(self, element_locator):
-        element = WebDriverWait(self.driver, 30).until(expected_conditions.visibility_of_element_located(element_locator))
+        element = WebDriverWait(self.driver, 30).until(
+            expected_conditions.visibility_of_element_located(element_locator))
         return element
